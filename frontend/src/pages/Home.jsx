b08689286a157c52d2e392915384caaf5faf3e10
Chirabrata Ghosal <chirabrataghosal9@gmail.com>
2026-05-18 23:41:55 +0530

    add home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container mx-auto p-4">
+            <div classname ='h-screen flex items-center justify-center'>
+                <div>
+                    <h2>get started with uber clone</h2>
+                    <p>Welcome to the Uber clone!</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;

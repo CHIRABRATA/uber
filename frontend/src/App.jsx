c554c4b9a91d2701c4cedc8512a848f461905017
Chirabrata Ghosal <chirabrataghosal9@gmail.com>
2026-05-20 01:25:13 +0530

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
+import AuthPage from './components/AuthPage';
+import UserLogin from './pages/Userlogin';
+import UserSignup from './pages/Usersignup';
+import CaptainLogin from './pages/Captainlogin';
+import CaptainSignup from './pages/Captainsignup';
 
 const App = () => {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<AuthPage defaultRole="user" defaultMode="signup" />} />
+      <Route path="/login" element={<UserLogin />} />
+      <Route path="/signup" element={<UserSignup />} />
+      <Route path="/captain/login" element={<CaptainLogin />} />
+      <Route path="/captain/signup" element={<CaptainSignup />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 };
 
 export default App;
